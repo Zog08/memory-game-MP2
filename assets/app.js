6c5e289f006008f95108a3efce2f23036a8793e1
Zoe Heathcote <zoe08@hotmail.co.uk>
2025-01-14 17:47:27 +0000

    feat: tidying code

--- assets/app.js
+++ assets/app.js
@@ -3,8 +3,8 @@ const counter = document.querySelector(".moves"); // moves counter div
 const time =  document.querySelector(".timer"); // timer div
 const accordion = document.querySelectorAll(".panel"); // target the panels for the drop down accordion on index page
 
-accordion.forEach(function (ele) {              // accordion function - with help from Laurence Svekis on Udemy 
-    console.log(ele);
+// accordion function - with help from Laurence Svekis on Udemy https://www.udemy.com/share/101XdM/
+accordion.forEach(function (ele) {              
     ele.addEventListener('click', toggleEle);
 })
 
@@ -26,29 +26,25 @@ let pairs = 0,
 
 function flipCard() {
     if (lockBoard) return;
-    if (this === firstCard) return; // if the card has been double clicked, return from the function
-    this.classList.add('flip'); // add flip card class when flipped first time
+    if (this === firstCard) return;     // if the card has been double clicked, return from the function
+    this.classList.add('flip');         // add flip card class when flipped first time
     moveCounter();
 
     if (!hasFlippedCard) {
-        // first flip
-        hasFlippedCard = true;
+        hasFlippedCard = true;          // first flip
         firstCard = this;
         return;
     }
-        // second card flip
-    secondCard = this;
+    secondCard = this;                  // second card flip
     checkForMatch();
 }
 
-function checkForMatch() {
-        // do the cards match?
+function checkForMatch() {              // do the cards match?
     let isMatch = firstCard.dataset.pic === secondCard.dataset.pic; 
        isMatch ? disableCards() : unflipCards(); // simplified if / else statement to ternary operator     
 }
 
-function disableCards() {
-     // match found. remove the event listener so cards cant be un-flipped.
+function disableCards() {               // match found. remove the event listener so cards cant be un-flipped.
         firstCard.removeEventListener('click', flipCard);
         secondCard.removeEventListener('click', flipCard);
         pairs++;
@@ -57,8 +53,7 @@ function disableCards() {
 }
 
 function unflipCards() {
-    lockBoard = true;
-     // if no match, remove class list of .flip to reset
+    lockBoard = true;                   // if no match, remove class list of .flip to reset
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
@@ -70,23 +65,14 @@ function resetBoard() {
     [hasFlippedCard, lockBoard] = [false, false];
     [firstCard, secondCard] = [null, null];
 }
-
-(function shuffle() {
+(function shuffle() {                   // Assign each card a new random position on the board
     cards.forEach(card => {
         let randomPos = Math.floor(Math.random() * 12);
         card.style.order = randomPos;
     });
-})(); // Immediately Invoked Function Expression, IIFE - invokes as soon as the page is loaded instead of calling on it during the program
+})(); 
 
-const finalScore = document.getElementById("finalScore");
-//const score = document.getElementsByClassName("score");
-function redirect() {
-    window.open("end.html", "_self"); // redirects you to end page to store score
-   //finalScore.innerText = `${(mins * 60) + secs + moves}`;
-    //console.log(score);
- }
-
-function startTimer () {
+function startTimer () {                // timer starts on first click
     interval = setInterval(() => {
         time.innerHTML = minutes + "mins " + seconds + "secs";
         seconds++;
@@ -103,32 +89,34 @@ function stopTimer() {
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
-function endGame() {
-    setTimeout(()=> {
-        stopTimer();
-        localStorage.setItem('mostRecentScore', moves);
-        //localStorage.setItem('mostRecentTime', secs);
-        redirect();
-        //endScore();
-        //alert(`You won in ${moves} moves, in ${minutes} minutes and ${seconds} seconds!`);     // template literal, displays winning moves and time in the alert box
-       // document.getElementById("you-won").innerText = `You won in ${moves} moves, in ${minutes} minutes and ${seconds} seconds!`
-    }, 300);
-}
-
-function moveCounter() {        // moves and timer learning, see readme for details (CogniVis AI youtube channel)
-    moves++;                    // add 1 to moves counter each flip. 
-    counter.innerHTML = moves;  // show in .moves html
-    if (moves == 1) {           // starts timer on first move, not on page load
-        startTimer();
-    }
-}
-
+// ----------------------------------------- game over
+const finalScore = document.getElementById("final-score");
 const username = document.getElementById("username");
-const saveScoreBtn = document.getElementById("saveScoreBtn");
+const saveScoreBtn = document.getElementById("save-score-btn");
 const mostRecentScore = localStorage.getItem('mostRecentScore'); //get most recent score from local storage saved at endgame()
 const highScoresList = document.getElementById("high-scores-list");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || []; // get high scores array from storage, parse items || of for first time it initialises an empty array
 const MAX_HIGH_SCORES = 5;
+
+function redirect() {
+    window.open("end.html", "_self"); // redirects you to end page to store score
+ }
+
+function endGame() {
+    setTimeout(()=> {
+        stopTimer();
+        localStorage.setItem('mostRecentScore', moves);
+        redirect();
+    }, 300);
+}
+
+function moveCounter() {            // moves and timer learning, CogniVis AI youtube channel, https://youtu.be/-5V0rUxJUbY?feature=shared
+    moves++;                        // add 1 to moves counter each flip. 
+    counter.innerHTML = moves;      // show in .moves html
+    if (moves == 1) {               // starts timer on first move, not on page load
+        startTimer();
+    }
+}
 
 finalScore.innerText = mostRecentScore;
 username.addEventListener("keyup", () => {
@@ -136,7 +124,6 @@ username.addEventListener("keyup", () => {
 });
 
 saveHighScore = e => {
-    console.log("clicked save");
     e.preventDefault();
 
 const score = {
@@ -145,11 +132,10 @@ const score = {
 };
 
 highScores.push(score);
-highScores.sort( (a,b) => a.score - b.score); // Order the score array with implicit arrow function. a-b sorts ascending
-highScores.splice(5); // only top5 scores in the array
+highScores.sort( (a,b) => a.score - b.score);       // Order the score array with implicit arrow function. a-b sorts ascending
+highScores.splice(5);                               // only top5 scores in the array
 localStorage.setItem("highScores", JSON.stringify(highScores)); // keeps the previous scores saved in local storage on refresh. turn hiscore to string
-window.location.reload(); // send you to home page once you click save?
-console.log(highScores);
+window.location.reload();                           // reloads page when you click save, to show updated hiscore board
 };
 
 highScoresList.innerHTML = highScores   
