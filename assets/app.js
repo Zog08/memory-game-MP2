21e058a343fd4f06ac9865b777eeb3d6b67492ff
Zoe Heathcote <zoe08@hotmail.co.uk>
2025-01-29 00:07:35 +0000

    feat:beautyfied the code

--- assets/app.js
+++ assets/app.js
@@ -1,19 +1,19 @@
 // get all memory cards
-const cards = document.querySelectorAll('.memory-card'); 
+const cards = document.querySelectorAll('.memory-card');
 // moves counter div
-const counter = document.querySelector(".moves"); 
+const counter = document.querySelector(".moves");
 // timer div
-const time =  document.querySelector(".timer"); 
+const time = document.querySelector(".timer");
 // target the panels for the drop down accordion on index page
-const accordion = document.querySelectorAll(".panel"); 
+const accordion = document.querySelectorAll(".panel");
 
 // accordion function on index page - with help from Laurence Svekis on Udemy, see README for more info and links
-accordion.forEach(function (ele) {              
+accordion.forEach(function (ele) {
     ele.addEventListener('click', toggleEle);
 });
 
 function toggleEle(e) {
-    accordion.forEach(function(ele) {
+    accordion.forEach(function (ele) {
         ele.classList.remove('active');
     });
     this.classList.toggle('active');
@@ -29,35 +29,35 @@ let pairs = 0,
     minutes = 0;
 
 function flipCard() {
-    if (lockBoard) return;              // 'lockboard' prevents multiple extra cards being flipped. From CodeSketchLab on Youtbue (see README for link)
-    if (this === firstCard) return;     // if the card has been double clicked, return from the function
-    this.classList.add('flip');         // add flip card class when flipped first time
+    if (lockBoard) return; // 'lockboard' prevents multiple extra cards being flipped. From CodeSketchLab on Youtbue (see README for link)
+    if (this === firstCard) return; // if the card has been double clicked, return from the function
+    this.classList.add('flip'); // add flip card class when flipped first time
     moveCounter();
 
     if (!hasFlippedCard) {
-        hasFlippedCard = true;          // first flip
+        hasFlippedCard = true; // first flip
         firstCard = this;
         return;
     }
-    secondCard = this;                  // second card flip
+    secondCard = this; // second card flip
     checkForMatch();
 }
 
-function checkForMatch() {              // do the cards match?
-    let isMatch = firstCard.dataset.pic === secondCard.dataset.pic; 
-       isMatch ? disableCards() : unflipCards(); // simplified if / else statement to ternary operator     
+function checkForMatch() { // do the cards match?
+    let isMatch = firstCard.dataset.pic === secondCard.dataset.pic;
+    isMatch ? disableCards() : unflipCards(); // simplified if / else statement to ternary operator     
 }
 
-function disableCards() {               // match found. remove the event listener so cards cant be un-flipped.
-        firstCard.removeEventListener('click', flipCard);
-        secondCard.removeEventListener('click', flipCard);
-        pairs++;
-        if (pairs == 6) endGame();
-        resetBoard();
+function disableCards() { // match found. remove the event listener so cards cant be un-flipped.
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    pairs++;
+    if (pairs == 6) endGame();
+    resetBoard();
 }
 
 function unflipCards() {
-    lockBoard = true;                   // if no match, remove class list of .flip to reset
+    lockBoard = true; // if no match, remove class list of .flip to reset
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
@@ -69,14 +69,14 @@ function resetBoard() {
     [hasFlippedCard, lockBoard] = [false, false];
     [firstCard, secondCard] = [null, null];
 }
-(function shuffle() {                   // Assign each card a new random position on the board
+(function shuffle() { // Assign each card a new random position on the board
     cards.forEach(card => {
         let randomPos = Math.floor(Math.random() * 12);
         card.style.order = randomPos;
     });
-})(); 
+})();
 
-function startTimer () {                // timer starts on first click
+function startTimer() { // timer starts on first click
     interval = setInterval(() => {
         time.innerHTML = minutes + "mins " + seconds + "secs";
         seconds++;
@@ -91,7 +91,7 @@ function stopTimer() {
     clearInterval(interval);
 }
 
-cards.forEach(card => card.addEventListener('click', flipCard));    // card flips on mouse click
+cards.forEach(card => card.addEventListener('click', flipCard)); // card flips on mouse click
 
 // Assigned constants for game over
 const finalScore = document.getElementById("final-score");
@@ -103,20 +103,20 @@ const highScores = JSON.parse(localStorage.getItem("highScores")) || []; // get 
 
 function redirect() {
     window.open("end.html", "_self"); // redirects you to end page to store score
- }
+}
 
 function endGame() {
-    setTimeout(()=> {
-        stopTimer();                // invoke stopTimer function to stop the time
-        localStorage.setItem('mostRecentScore', moves);     // save score to local storage
-        redirect();                 // redirect to end game
+    setTimeout(() => {
+        stopTimer(); // invoke stopTimer function to stop the time
+        localStorage.setItem('mostRecentScore', moves); // save score to local storage
+        redirect(); // redirect to end game
     }, 300);
 }
 
-function moveCounter() {            // moves and timer learning, CogniVis AI youtube channel, see README for more info and links
-    moves++;                        // add 1 to moves counter each flip. 
-    counter.innerHTML = moves;      // show in .moves html
-    if (moves == 1) {               // starts timer on first move, not on page load
+function moveCounter() { // moves and timer learning, CogniVis AI youtube channel, see README for more info and links
+    moves++; // add 1 to moves counter each flip. 
+    counter.innerHTML = moves; // show in .moves html
+    if (moves == 1) { // starts timer on first move, not on page load
         startTimer();
     }
 }
@@ -126,23 +126,23 @@ username.addEventListener("keyup", () => {
     saveScoreBtn.disabled = !username.value; // disable the save button if nothing is typed in the name input, listen for keyup
 });
 
-saveHighScore = e => {              // saveHighScore is an onclick event in end.html
+saveHighScore = e => { // saveHighScore is an onclick event in end.html
     e.preventDefault();
 
-const score = {
-    name: username.value,
-    score: mostRecentScore
+    const score = {
+        name: username.value,
+        score: mostRecentScore
+    };
+
+    highScores.push(score);
+    highScores.sort((a, b) => a.score - b.score); // Order the score array with implicit arrow function. a-b sorts ascending
+    highScores.splice(5); // only top5 scores in the array
+    localStorage.setItem("highScores", JSON.stringify(highScores)); // keeps the previous scores saved in local storage on refresh. turn hiscore to string
+    window.location.reload(); // reloads page when you click save, to show updated hiscore board
 };
 
-highScores.push(score);
-highScores.sort( (a,b) => a.score - b.score);       // Order the score array with implicit arrow function. a-b sorts ascending
-highScores.splice(5);                               // only top5 scores in the array
-localStorage.setItem("highScores", JSON.stringify(highScores)); // keeps the previous scores saved in local storage on refresh. turn hiscore to string
-window.location.reload();                           // reloads page when you click save, to show updated hiscore board
-};
-
-highScoresList.innerHTML = highScores               // Adds name and hi-score to the score board on end page, if in top 5 score
-        .map(score => {
-            return `<li class="high-score">${score.name} - ${score.score}</li>`;
-        })
+highScoresList.innerHTML = highScores // Adds name and hi-score to the score board on end page, if in top 5 score
+    .map(score => {
+        return `<li class="high-score">${score.name} - ${score.score}</li>`;
+    })
     .join("");
